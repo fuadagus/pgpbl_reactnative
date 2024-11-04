bf99eddd45bc4006984184647324d7fc73151644
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-11-04 08:22:28 +0700

    start to style the app

--- App.tsx
+++ App.tsx
@@ -76,17 +76,16 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Nama">
+           Fuad Agus Salim
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section title="NIM">
+            22/506149/SV/22084
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
+          <Section title="Kelas">
+            PGPBL - B 
           </Section>
-          <Section title="Learn More">
+          <Section title="Follow Me for More">
             Read the docs to discover what to do next:
           </Section>
           <LearnMoreLinks />
